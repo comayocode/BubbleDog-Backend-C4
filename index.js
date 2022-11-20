a8324f685b2766fb075e68424e7fb1b2b11d1bbd
Milfer Muñoz <milfer0516@gmail.com>
2022-11-19 19:33:34 -0500

    Agrego la carpeta src y las dependencias express,cors,dotenv,jsonwebtoken,bcryptjs

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const Server = require("./src/server/server");
+
+const server = new Server();
+
+server.listen();
---
+++ src/server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(
+        `Servidor corriendo en el puerto http://localhost:${this.port}`
+      );
+    });
+  }
+}
+
+module.exports = Server;
